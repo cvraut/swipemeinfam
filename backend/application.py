0053588bf74325abc458bf0e0017e68617df3682
Fern <fernanlukban@gmail.com>
2017-05-05 16:12:02 -0700

    final backend

--- backend/application.py
+++ backend/application.py
@@ -1,10 +1,14 @@
 from flask import Flask
 import routes
 
-application = Flask(__name__)
+application = Flask(__name__)   
 
 for route in routes.ALL_ROUTES:
     application.add_url_rule(rule=route.url, view_func=route.func, methods=route.methods)
 
+@application.route('/')
+def index():
+    return "hello world!"
+
 if __name__ == '__main__':
-    application.run()
+    application.run(port=80)
