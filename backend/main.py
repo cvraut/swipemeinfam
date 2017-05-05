90e916c7e0c208f58db5740b562b853a9b69ba58
Fern <fernanlukban@gmail.com>
2017-05-04 22:44:30 -0700

    fixed head

--- backend/main.py
+++ backend/main.py
@@ -7,8 +7,4 @@ for route in routes.ALL_ROUTES:
     app.add_url_rule(rule=route.url, view_func=route.func, methods=route.methods)
 
 if __name__ == '__main__':
-<<<<<<< HEAD
     app.run()
-=======
-    app.run(debug=True, port=5000)
->>>>>>> 10336a10c96de8112c90926f33fe3e673bcea581
