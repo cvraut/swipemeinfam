59ddfefeee88aef132df6df655abc125c6bac1ac
fernanlukban <fernanlukban@gmail.com>
2017-05-05 17:08:03 -0700

    added some stuff

--- backend/application.py
+++ backend/application.py
@@ -1,7 +1,9 @@
 from flask import Flask
 import routes
+from flask_cors import CORS, cross_origin
 
 application = Flask(__name__)   
+CORS(application)
 
 for route in routes.ALL_ROUTES:
     application.add_url_rule(rule=route.url, view_func=route.func, methods=route.methods)
