e705eb847c9ab139fdccf9f6bc7712b4386933f2
kaleo-sato <kaleo.sato@gmail.com>
2017-05-16 20:20:51 -0700

    Add files via upload
    Fixed sorting, but still need to adjust "Filter by"

--- cli.py
+++ cli.py
@@ -3,10 +3,12 @@ from copy import copy
 from datetime import datetime
 import smtplib
 from email.mime.text import MIMEText
+import json
+import math
 
 
-DAYS = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
-DEFAULT_SORT = ['times', 'credibility_index', 'swipes', 'cost']
+DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+DEFAULT_SORT = ['times', 'swipes', 'cost'] # credibility_index will always be sorted as second priority
 
 def run_gui():
 
@@ -19,7 +21,7 @@ def run_gui():
     while True:
         print('''
         commands:
-    ct Day:HH : change time
+    ct Day-HH : change time
     vp        : view pippin menu
     va        : view anteatery menu
     sp <str>  : select person from list
@@ -37,7 +39,8 @@ def run_gui():
         command = input('Enter a command -> ')
 
         if command.startswith('ct'):
-            time = command[3:] + ':00'
+            day = command[3:command.index('-')]
+            time = command[command.index('-')+1:]
         elif command == 'vp':
             display_pippin_menu()
         elif command == 'va':
@@ -51,13 +54,12 @@ def run_gui():
         elif command == 'f':
             filters = filter_table()
         elif command == 'uf':
-            time, day, sort_by, people_shown = reset()
+            time, day, sort_by, people_shown,filters = reset()
         elif command == '':
             people_shown += 5
         elif command == 'q':
             break # GRADY IS TRIGGERED
-
-
+    
 
 def display_pippin_menu():
     print("https://uci.campusdish.com/Commerce/Catalog/Menus.aspx?LocationId=4832")    
@@ -74,24 +76,24 @@ def select_person(ucinetid: str):
     email = "{}@uci.edu".format(ucinetid)
         #name = apirequest.get_request({ucinetid: name}) # how the fuck does get_request work?
         # what are the params i have to input?
-        html_string = """\
-            <html>
-            <head></head>
-            <body>
-            <p>
-            Hello Student!<br>
-            Thank you for signing up for Antfeeder!<br>
-            <a href="http://www.google.com">Click here to confirm you registration</a>.
-            </p>
-            </body>
-            </html>
-            """#.format(name)
-        test_server = "cdxu@uci.edu" #replace with whatever email we send from later
-        test_email = "craut@uci.edu" #using chinmay's email as a guinea pig
-        msg = MIMEText(html_string, 'html')
-        server = smtplib.SMTP('localhost', 1025) #change the domain to our SMTP host eventually
-        server.send_mail(test_server, test_email, msg.as_string())
-        server.quit()
+    html_string = """\
+                <html>
+                <head></head>
+                <body>
+                <p>
+                Hello Student!<br>
+                Thank you for signing up for Antfeeder!<br>
+                <a href="http://www.google.com">Click here to confirm you registration</a>.
+                </p>
+                </body>
+                </html>
+                """#.format(name)
+    test_server = "cdxu@uci.edu" #replace with whatever email we send from later
+    test_email = "craut@uci.edu" #using chinmay's email as a guinea pig
+    msg = MIMEText(html_string, 'html')
+    server = smtplib.SMTP('localhost', 1025) #change the domain to our SMTP host eventually
+    server.send_mail(test_server, test_email, msg.as_string())
+    server.quit()
 
 def create_account():
     # Create the account and put it in the DB
@@ -171,7 +173,7 @@ def create_account():
     apirequest.post_request(ucinetid, infodict)
 
 def change_sorting_priority():
-    priority = input("Enter in the new field you would like to prioritize in the table -> ")
+    priority = input("Enter in the new field you would like to prioritize in the table -> ").lower()
     return _get_sorting_preferences([priority])
 
 def filter_table():
@@ -185,9 +187,12 @@ def filter_table():
     if name != '':
         result['name'] = name
     if time != '':
-        result['time'] = time
+        result['times'] = time
     if location != '':
-        result['location'] = location
+        if location.lower() == 'pippin':
+            result['pippin'] = True
+        elif location.lower() == 'anteatery':
+            result['anteatery'] = True
     if cost != '':
         result['cost'] = cost
     
@@ -199,23 +204,25 @@ def reset():
 
 
 
-def display_people(day:str, time:str, people_shown, sort_by=['times', 'credibility_index', 'swipes', 'cost'], filters={}):
+def display_people(day:str, time:str, people_shown, sort_by=['times', 'swipes', 'cost'], filters={}):
     sort_by = _get_sorting_preferences(sort_by)
     print('                        Swipe Me In              ')
     print('               Listings for ' + day + ' at ' + time)
+    print('Sorting by: ', str(sort_by))
+    print('{:<20}{:<20}{:<20}{:<20}{:<20}{:<20}'.format('Name', 'Time', 'Pippin?', 'Anteatery?', 'Cost', 'Swipes'))
 
-    print('{:<10}{:<10}{:<10}{:<15}{:<10}{:<10}'.format('Name', 'Time', 'Pippin?', 'Anteatery?', 'Cost', 'Swipes'))
-
-    json_text = apirequest.get_request({'times':time})
+    #json_text = apirequest.get_request({'times':time})
+    json_data = open('dankbase.json')
+    json_text = json.load(json_data)
     result = ''
     count = 0
-    for user in sorted(json_text['users'], key=(lambda x:(x[sort_by[0]],x[sort_by[1]],x[sort_by[2]],x[sort_by[3]]))):
+    for person in sorted(json_text, key=(lambda x:(sort_functions(day, time, sort_by[0], json_text[x][sort_by[0]]),-1* _cred_ind(json_text[x]['success'], json_text[x]['failure'], json_text[x]['swipes'], json_text[x]['response_time']),sort_functions(day, time, sort_by[1], json_text[x][sort_by[1]]), sort_functions(day, time, sort_by[2], json_text[x][sort_by[2]])))):
         if len(filters) == 0:
-            result += '{:<10}{:<10}{:<10}{:<15}{:<10}{:<10}'.format(user['name'], str(user['times']['wd_times']), str(user['places']['pippin']), str(user['places']['anteatery']), str(user['cost']), str(user['swipes'])) + '\n'
+            result += '{:<20}{:<20}{:<20}{:<20}{:<20}{:<20}'.format(json_text[person]['name'], display_next_time(day, time, json_text[person]['times']), str(json_text[person]['pippin']), str(json_text[person]['anteatery']), str(json_text[person]['cost']), str(json_text[person]['swipes'])) + '\n'
             count += 1
         else:
-            if all(user[key] == filters[key] for key in filters):                  
-                result += '{:<10}{:<10}{:<10}{:<15}{:<10}{:<10}'.format(user['name'], str(user['times']['wd_times']), str(user['places']['pippin']), str(user['places']['anteatery']), str(user['cost']), str(user['swipes'])) + '\n'
+            if all(json_text[person][key] == filters[key] for key in filters):                  
+                result += '{:<20}{:<20}{:<20}{:<20}{:<20}{:<20}'.format(json_text[person]['name'],display_next_time(day, time, json_text[person]['times']), str(json_text[person]['pippin']), str(json_text[person]['anteatery']), str(json_text[person]['cost']), str(json_text[person]['swipes'])) + '\n'
                 count += 1
 
         if count == people_shown:
@@ -223,8 +230,41 @@ def display_people(day:str, time:str, people_shown, sort_by=['times', 'credibili
 
     print(result)
 
+
+def sort_functions(day, time, name, value):
+    if name == 'times':
+        return _time_sort(day, time, value)
+    elif name == 'swipes':
+        return -1 * int(value)
+    elif name == 'cost':
+        return int(value)
+        
+
+def _time_sort(day, time, all_times):
+    for t in sorted(all_times[day]):
+        if t - int(time[:time.index(':')]) >= 0:
+            return t - int(time[:time.index(':')])
+    return 24
+
+def display_next_time(day, time, all_times):
+    for t in sorted(all_times[day]):
+        if t - int(time[:time.index(':')]) >= 0:
+            return day + ', ' + str(t) + ':00'
+    current_day = (DAYS.index(day)+1) % 7
+    for i in range(6):
+        if len(all_times[DAYS[current_day]]) == 0:
+            current_day = (current_day+1)%7
+        else:
+            return DAYS[current_day] + ', ' + str(all_times[DAYS[current_day]][0]) + ':00'
+
+def _cred_ind(successes, failures, swipes, resp_time):
+    first_frac = (successes+1)/(failures+1)
+    second_frac = (swipes/(math.sqrt(resp_time+1)))
+
+    return first_frac * second_frac
+
 def _get_sorting_preferences(sort_by):
-    if len(sort_by) == 4:
+    if len(sort_by) == 3:
         return sort_by
 
     if len(sort_by) == 1:
@@ -232,8 +272,6 @@ def _get_sorting_preferences(sort_by):
         sorter = copy(DEFAULT_SORT)
         priority = sorter.pop(index)
         sorter.insert(0, priority)
-        credibility = sorter.pop(sorter.index('credibility_index'))
-        sorter.insert(1, credibility)
         return sorter
 
     return DEFAULT_SORT
